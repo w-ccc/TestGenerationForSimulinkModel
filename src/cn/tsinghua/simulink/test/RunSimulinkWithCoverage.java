18ae46a05015612fbad1633d20492e3d334e0905
yixiao yang <yangyixiaofirst@163.com>
2020-02-23 05:33:15 +0800

    small change.

--- src/cn/tsinghua/simulink/test/RunSimulinkWithCoverage.java
+++ src/cn/tsinghua/simulink/test/RunSimulinkWithCoverage.java
@@ -1,6 +1,5 @@
 package cn.tsinghua.simulink.test;
 
-import java.io.File;
 import java.util.List;
 import java.util.concurrent.Future;
 
@@ -27,33 +26,35 @@ public class RunSimulinkWithCoverage {
 		}
 		System.out.println();
 		System.out.println("Simulink Model Loaded");
+		eng.eval("all_cov_value = -1.0;");
 	}
 
 	public RunInfo CoverageRun(List<String> input_port_values) throws Exception {
-		File all_cv_file = new File(model_name + "_cv.cvt");
-		if (all_cv_file.exists()) {
-			eng.eval("[a_tests, a_cvs] = cvload('" + model_name + "_cv');");
-			eng.eval("all_cv = a_cvs{1};");
-			eng.eval("all_cov = decisioninfo(all_cv, '" + model_name + "');");
-			eng.eval("all_cov_value = all_cov(1) / all_cov(2);");
-		}
+//		File all_cv_file = new File(model_name + "_cv.cvt");
+//		if (all_cv_file.exists()) {
+//			eng.eval("[a_tests, a_cvs] = cvload('" + model_name + "_cv');");
+//			eng.eval("all_cv = a_cvs{1};");
+//			eng.eval("all_cov = decisioninfo(all_cv, '" + model_name + "');");
+//			eng.eval("all_cov_value = all_cov(1) / all_cov(2);");
+//		}
 		System.out.println("Running Simulation...");
 		String vals = StringUtils.join(input_port_values.iterator(), ",");
 //		System.out.println("vals:" + vals);
 		eng.eval("cvdata = cvsim('test', 'LoadExternalInput', 'on', 'ExternalInput', '" + vals + "');");
 		eng.eval("after_all_cv = cvdata;");
-		if (all_cv_file.exists()) {
+		if ((double)eng.getVariable("all_cov_value") >= 0.0) {
 			eng.eval("after_all_cv = all_cv + after_all_cv;");
 		}
 		eng.eval("after_all_cov = decisioninfo(after_all_cv, '" + model_name + "');");
 		eng.eval("after_all_cov_value = after_all_cov(1) / after_all_cov(2);");
 		SeedKind kind = SeedKind.NotInterested;
-		if (!all_cv_file.exists() || (all_cv_file.exists() && (double)eng.getVariable("all_cov_value") < (double)eng.getVariable("after_all_cov_value"))) {
+		if ((double)eng.getVariable("all_cov_value") < (double)eng.getVariable("after_all_cov_value")) {
 			// seed is interesting
-			eng.eval("all_cov_value = after_all_cov_value;");
 			kind = SeedKind.Interesting;
 		}
-		eng.eval("cvsave('" + model_name + "_cv', after_all_cv);");
+		eng.eval("all_cov_value = after_all_cov_value;");
+		eng.eval("all_cv = after_all_cv;");
+//		eng.eval("cvsave('" + model_name + "_cv', after_all_cv);");
 		System.out.println("Simulation Over");
 		return new RunInfo(kind, (double)eng.getVariable("all_cov_value"));
 	}
